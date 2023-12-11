d3fd51fda16329d464bdc5cd3607aa1c9fc44b33
Usuario <juanjolopezromero@gmail.com>
2023-12-11 10:08:26 +0100

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
